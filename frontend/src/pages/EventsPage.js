0b7e6f4b50b704e4b84664dbd1d20b9bb6232bdd
asrivastava2302 <anubhavgoblin2302@gmail.com>
2024-03-02 12:33:53 +0530

    example

---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+    {
+        id: 'e1',
+        title: 'Some Event'
+    },
+    {
+        id: 'e2',
+        title: 'Another Event'
+    }
+]
+
+function EventsPage() {
+    return(
+        <>
+        <h1>Event Page</h1>
+        {DUMMY_EVENTS.map((event)=><li key={event.id}><Link to={event.id}>{event.title}</Link></li>)}
+        </>
+    )
+}
+
+export default EventsPage;
